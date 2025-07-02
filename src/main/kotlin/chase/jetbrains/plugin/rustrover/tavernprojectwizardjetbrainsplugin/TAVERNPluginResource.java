2446af0a50fca9c731d7d331566690ce72affdb4
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-02 15:38:15 -0400

    Add TAVERNPluginResource with plugin icon configuration

---
+++ src/main/kotlin/chase/jetbrains/plugin/rustrover/tavernprojectwizardjetbrainsplugin/TAVERNPluginResource.java
@@ -0,0 +1,9 @@
+package chase.jetbrains.plugin.rustrover.tavernprojectwizardjetbrainsplugin;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class TAVERNPluginResource {
+    public static final Icon ICON = IconLoader.getIcon("/tavern-icon.png", TAVERNPluginResource.class);
+}
